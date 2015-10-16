5db7de755ed183fd5d1991605d6249f628acaf85
Jon Poehlman <jon.poehlman@gmail.com>
2015-10-16 01:47:16 +0000

    pony and flash

--- main.rb
+++ main.rb
@@ -5,17 +5,16 @@ require "sass"
 require 'dm-core'
 require 'dm-migrations'
 require './song'
+require 'sinatra/flash'
+require "pony"
 
 configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  DataMapper.setup(:default, "sqlite3://#{ Dir.pwd}/development.db")
 end
 
 configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
+  DataMapper.setup(:default, ENV[' DATABASE_URL'])
 end
-
-
-get('/styles.css'){ scss :styles }
 
 configure do
   enable :sessions
@@ -27,6 +26,48 @@ configure :development do
   set :bind, '0.0.0.0'
   set :port, 3000
 end
+
+before do
+  set_title
+end
+
+helpers do
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+    end.join
+  end
+
+  def current?(path='/')
+    (request.path==path || request.path==path+'/') ? "current" : nil
+  end
+
+  def set_title
+    @title ||= "Songs By Sinatra"
+  end
+
+  def send_message
+    Pony.mail({
+      :from => params[:name] + "<" + params[:email] + ">",
+      :to => 'poehlman356@gmail.com',
+      :subject => params[:name] + " has contacted you",
+      :body => params[:message],
+      :port => '587',
+      :via => :smtp,
+      :via_options => {
+        :address              => 'smpt.gmail.com',
+        :port                 => '587',
+        :enable_starttls_auto => true,
+        :user_name            => 'poehlman356',
+        :password             => 'chona-356',
+        :authentication       => :plain,
+        :domain               => 'localhost.localdomain'
+      }
+      })
+  end
+end
+
+get('/styles.css'){ scss :styles }
 
 get '/login' do
   slim :login
@@ -65,3 +106,8 @@ not_found do
 	slim :not_found
 end
 
+post '/contact' do
+  send_message
+  flash[:notice] = "Thank you for your message. We'll be in touch soon."
+  redirect to('/')
+end
